46aa6abea2829a861a6109f637e3cada81266cf3
whosnw <almakts97@gmail.com>
2026-05-04 17:53:45 -0500

    Update Confeti.js

--- Confeti.js
+++ Confeti.js
@@ -44,7 +44,7 @@ function crearConfeti() {
 // 3. Dibujar y Mover Confeti
 //
 function animarConfeti() {
-  ctxConfeti.fillStyle = "rgba(255, 182, 193, 1)"; // Fondo Rosa Pastel
+  ctxConfeti.fillStyle = "rgba(8, 10, 124, 0.92)"; // Fondo Rosa Pastel
   ctxConfeti.fillRect(0, 0, ancho, alto);
 
   for (let i = 0; i < confetis.length; i++) {
